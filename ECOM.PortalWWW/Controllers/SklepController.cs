2bc490a327e8e853bf54a211c0742ae2459a0d9f
kjszywala <kjszywala@protonmail.com>
2023-04-30 17:16:13 +0100

    Partial views

---
+++ ECOM.PortalWWW/Controllers/SklepController.cs
@@ -0,0 +1,39 @@
+﻿using Firma.Data.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECOM.PortalWWW.Controllers
+{
+    public class SklepController : Controller
+    {
+        private readonly FirmaContext _context;
+
+        public SklepController(FirmaContext context)
+        {
+            _context = context;
+        }
+        
+        /// w zmiennej id bedzie przekazywane id categorii kliknietego rodzaju towaru.
+        public async Task<IActionResult> Index(int? id)
+        {
+            // Za pomoca view baga przekazujemy do widoku wszystkie rodzaje towarow.
+            ViewBag.Rodzaje = await _context.Rodzaj.ToListAsync();
+            // Przy pierwszym wejsciu do sklepu id rodzaju towaru bedzie puste, zatem domyslnie podstawimy tam pierwszy rodzaj.
+            if(id == null)
+            {
+                var pierwszy = await _context.Rodzaj.FirstAsync();
+                id = pierwszy.IdRodzaju;
+            }
+            // do widoku przekazujemy wszystkie to wary danego kliknietego rodzaju.
+            return View(await _context.Towar.Where(item =>item.RodzajId == id).ToListAsync());
+        }
+
+        //to jest funkcja ktora wystawia dane do widoku wyswietlajacego szczegoly towaru.
+        public async Task<IActionResult> Details(int id) // w parametrze id jest okreslone id towaru ktorego szczegoly mamy wyswietlic.
+        {
+            ViewBag.Rodzaje = await _context.Rodzaj.ToListAsync();
+            //do widoku przekazujemy towar o danym kliknietym id.
+            return View(await _context.Towar.FirstOrDefaultAsync(item => item.IdTowaru == id));
+        }
+    }
+}
--- ECOM.PortalWWW/Program.cs
+++ ECOM.PortalWWW/Program.cs
@@ -1,8 +1,13 @@
+using Firma.Data.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<FirmaContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FirmaContext") ?? throw new InvalidOperationException("Connection string 'ECOMContext' not found.")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,7 +17,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
